d0e9162f2c1dc33cad98e1e3f94836d20e09f340
NilsVreman <nils.vreman@gmail.com>
2022-06-28 15:46:36 +0200

    Fixed automaton doctext to work with documenter

--- src/automaton_set.jl
+++ src/automaton_set.jl
@@ -207,21 +207,21 @@ NOTE: The function handles both single constraints and sets of constraints.
 julia> build_automaton(AnyHitConstraint(1, 3))
 Automaton{Int64} with 3 vertices:
 {
-        WordVertex{Int64}(100 => ---, 001)
-        WordVertex{Int64}(010 => 100, 001)
-        WordVertex{Int64}(001 => 010, 001)
+	WordVertex{Int64}(100 => ---, 001)
+	WordVertex{Int64}(010 => 100, 001)
+	WordVertex{Int64}(001 => 010, 001)
 } with head: WordVertex{Int64}(1 => 10, 1)
 
 julia> build_automaton(Set([AnyHitConstraint(1, 3), RowHitConstraint(2, 6)]))
 Automaton{Int64} with 7 vertices:
 {
-        WordVertex{Int64}(001101 => 011010, 000011)
-        WordVertex{Int64}(000110 => 001100, 001101)
-        WordVertex{Int64}(011001 => ------, 000011)
-        WordVertex{Int64}(011010 => ------, 110101)
-        WordVertex{Int64}(001100 => ------, 011001)
-        WordVertex{Int64}(000011 => 000110, 000011)
-        WordVertex{Int64}(110101 => ------, 000011)
+	WordVertex{Int64}(001101 => 011010, 000011)
+	WordVertex{Int64}(000110 => 001100, 001101)
+	WordVertex{Int64}(011001 => ------, 000011)
+	WordVertex{Int64}(011010 => ------, 110101)
+	WordVertex{Int64}(001100 => ------, 011001)
+	WordVertex{Int64}(000011 => 000110, 000011)
+	WordVertex{Int64}(110101 => ------, 000011)
 } with head: WordVertex{Int64}(11 => 110, 11)
 ```
 """ build_automaton
